6b263aaabb8e8e0d1ebc415d96956bb6b04f0d40
iTeffa <97382411+lifesheets@users.noreply.github.com>
2025-04-23 04:58:17 +0300

    Class GeoMapSvgGenerator

---
+++ src/GeoMapSvgGenerator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Flowaxy;
+
+/**
+ * Class GeoMapSvgGenerator
+ *
+ * Generates an interactive SVG map from GeoJSON data and custom markers.
+ *
+ * @package Flowaxy
+ */
+
+class GeoMapSvgGenerator
+{
+}
